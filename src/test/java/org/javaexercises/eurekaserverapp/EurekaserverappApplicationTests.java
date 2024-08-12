67ff65b9a28aba5fb2b854b0a1ee809e65b88029
cirosouza <ciro_lut@hotmail.com>
2024-08-11 21:48:50 -0300

    First commit

---
+++ src/test/java/org/javaexercises/eurekaserverapp/EurekaserverappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaexercises.eurekaserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
